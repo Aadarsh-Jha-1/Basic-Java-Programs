c7688efd30e1aac0280dacc7b83f3b4ece5be9af
AADARSH JHA <145737043+Aadarsh-Jha-1@users.noreply.github.com>
2024-04-16 08:59:25 +0530

    Create 07 DoubleToInt.java

---
+++ 07 DoubleToInt.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static void main(String[] args) {
+        double starDistance = 142.5; // Distance in light years as a double
+        int roundedDistance = (int)starDistance; // There's an error related to types here
+        
+        String message = "The star is " + roundedDistance + " light years away.";
+        System.out.println(message); // Is the printed message right?
+    }
+}
