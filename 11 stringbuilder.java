c091a3f5da7847af582f873c75dbcfb787a23c33
AADARSH JHA <145737043+Aadarsh-Jha-1@users.noreply.github.com>
2024-04-20 13:22:59 +0530

    Create 11 stringbuilder.java

---
+++ 11 stringbuilder.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static void main(String[] args) {
+        //String socialMessage = "Hello, " + "Explorer! " + "Hope you're enjoying the Java Journey to the Stars course.";
+        StringBuilder socialMessage = new StringBuilder() ;
+        socialMessage.append("Hello, ").append("Explorer! ").append("Hope you're enjoying the Java Journey to the Stars course.");
+        
+        System.out.println(socialMessage);
+    }
+}
