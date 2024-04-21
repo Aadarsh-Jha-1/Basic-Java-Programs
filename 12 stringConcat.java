a2f46e9d75f0ae863e865f7aeb86ae4c05f474ac
AADARSH JHA <145737043+Aadarsh-Jha-1@users.noreply.github.com>
2024-04-21 14:15:02 +0530

    Create 12 stringConcat.java

---
+++ 12 stringConcat.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static void main(String[] args) {
+        String spaceExplorer = "Cosmo";
+        int discoveredPlanets = 7;
+        
+        // TODO: Use the '+' operator to concatenate spaceExplorer, discoveredPlanets and helper message strings to create a report.
+        String report1 = spaceExplorer+" discovered " + discoveredPlanets+" planets." ; // Replace this with your concatenation code using '+'
+
+        String excitement = " This is amazing!";
+        
+        // TODO: Complete the following line to concatenate the excitement message to the report using the 'concat' method.
+        String report2 = report1.concat(excitement);
+        
+        System.out.println(report2);
+    }
+}
